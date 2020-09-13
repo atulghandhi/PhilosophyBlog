b6c50b6cd44163bed51f2a36dfd972dc146bfba7
bobyghandhi <bobyghandhi@gmail.com>
2020-09-13 16:23:27 +0100

    new branch with if statement based form validation

--- JavaScript/LogInFormValidator.js
+++ JavaScript/LogInFormValidator.js
@@ -20,7 +20,7 @@ function showSuccess(field) {
 
 //Check if email is valid
 function validateEmail(email){
-    const REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return REGEX.test(String(email).toLowerCase());
 }
 
