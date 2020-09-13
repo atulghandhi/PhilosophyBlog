2bd32ed3c19a7f67a094d74cd9585ab5e438fd24
bobyghandhi <bobyghandhi@gmail.com>
2020-09-13 16:16:07 +0100

    Added some form validation

--- JavaScript/LogInFormValidator.js
+++ JavaScript/LogInFormValidator.js
@@ -4,15 +4,24 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-function showError(username, usernameIsRequired) {
-    const formControl = username.parentElement;
+//If there is error in one of the fields
+function showError(field, errorMessage) {
+    const formControl = field.parentElement;
     formControl.className = 'form-control error';
     const small = formControl.querySelector('small');
-    small.innerText = usernameIsRequired;
+    small.innerText = errorMessage;
 }
 
-function showSuccess(username) {
-    //
+//If field is filled in correctly
+function showSuccess(field) {
+    const formControl = field.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//Check if email is valid
+function validateEmail(email){
+    const REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return REGEX.test(String(email).toLowerCase());
 }
 
 //Event listeners
@@ -25,4 +34,23 @@ form.addEventListener('submit', function (e) {
         showSuccess(username);
     }
 
+    if (email.value === '') {
+        showError(email, 'Email is required');
+    } else if (!validateEmail(email.value)){
+        showError(email, 'Email is invalid');
+    } else {
+        showSuccess(email);
+    }
+
+    if (password.value === '') {
+        showError(password, 'Password is required');
+    } else {
+        showSuccess(password);
+    }
+
+    if (password2.value === '') {
+        showError(password2, 'Password confirmation is required');
+    } else {
+        showSuccess(password2);
+    }
 });
