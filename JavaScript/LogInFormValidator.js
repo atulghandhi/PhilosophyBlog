9b3d4f8368a621c2039d354b07ef9513506a213c
bobyghandhi <bobyghandhi@gmail.com>
2020-09-19 12:36:32 +0100

    Completed sign up form validation

--- JavaScript/LogInFormValidator.js
+++ JavaScript/LogInFormValidator.js
@@ -18,16 +18,48 @@ function showSuccess(field) {
     formControl.className = 'form-control success';
 }
 
-//Check if email is valid
-function validateEmail(email){
-    const REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return REGEX.test(String(email).toLowerCase());
+//Capitalise the first letter of input string
+function capitalise(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-//Validate input fields
-function validateInput(fieldsArray) {
-    for (const field of fieldsArray) {
-        console.log(field.value);
+//Check if email is valid
+function checkEmail(field){
+    const REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if(REGEX.test(field.value.trim())){
+        showSuccess(field)
+    } else {
+        showError(field, "Email invalid")
+    }
+}
+
+//Check input field are required length
+function checkLength(field, min, max) {
+    if(field.value.length < min){
+        showError(field, `${capitalise(field.id)} must be at least ${min} characters`)
+    } else if(field.value.length > max){
+        showError(field, `${capitalise(field.id)} must be fewer than ${max} characters`)
+    } else {
+        showSuccess(field);
+    }
+}
+
+//Validate input strings are not empty
+function checkIfEmpty(fieldsArray) {
+    fieldsArray.forEach(function (field) {
+        if(field.value.trim() === ''){
+            showError(field, `${capitalise(field.id)} is required...`)
+        } else {
+            showSuccess(field);
+        }
+    })
+}
+
+//Validate passwords match
+function checkFieldsMatch(field, field2) {
+    if(field.value !== field2.value) {
+        showError(field2, `${capitalise(field.id)} and ${capitalise(field2.id)} must match...`)
     }
 }
 
@@ -35,5 +67,9 @@ function validateInput(fieldsArray) {
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
-    validateInput([username, email, password, password2]);
+    checkIfEmpty([username, email, password, password2]);
+    checkLength(username, 6, 20);
+    checkLength(password, 8, 20);
+    checkEmail(email)
+    checkFieldsMatch(password, password2);
 });
